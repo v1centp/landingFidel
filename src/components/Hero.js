6540de2929542d274a4ba156ecb377606ce69a32
v1centp <vincent.porret@hotmail.com>
2023-09-01 18:19:30 +0200

    deploy

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -21,12 +21,12 @@ const Hero = ({ appType, tagLine, mainActionText, extraActionText }) => {
         <div id="faq" className=" text-3xl font-semibold text-center text-white lg:font-bold">1st November 2023 </div>
         <div className="relative z-50 flex flex-col items-center justify-between h-48 lg:space-x-8 pt-7 lg:pt-0 lg:flex-row lg:justify-between lg:w-90">
           <button
-            className="pt-3 pb-3 pl-12 pr-12 text-2xl font-semibold text-center text-white transition-all bg-green-400 rounded-full shadow-2xl lg:ml-5 hover:bg-green-500 focus:outline-none ring-4 ring-white lg:ring-2 lg:font-medium "
+            className="pt-3 pb-3 pl-12 pr-12 text-2xl font-semibold text-center text-white transition-all bg-green-400 rounded-full shadow-2xl lg:ml-5 hover:bg-green-500 focus:outline-none  ring-white ring-2 lg:font-medium "
           >
             {mainActionText}
           </button>
           <button
-            className="pt-3 pb-3 text-2xl font-semibold text-center text-green-400 transition-all rounded-full shadow-2xl lg:mr-5 hover:text-green-400 hover:bg-gray-50 pl-11 pr-11 bg-gray-50 focus:outline-none ring-4 ring-green-400 lg:font-medium lg:text-gray-50 lg:bg-opacity-0 lg:ring-2 lg:ring-white mb-2"
+            className="pt-3 pb-3 text-2xl font-semibold text-center transition-all rounded-full shadow-2xl lg:mr-5 hover:text-green-400 hover:bg-gray-50 pl-11 pr-11 bg-gray-50 focus:outline-none hover:ring- ring-white lg:font-medium text-gray-50 bg-opacity-0 ring-2 mb-2"
           >
             {extraActionText}
           </button>
